3a9fc39552e38068ca8451cb5b31e0ea24a872fb
unknown <celesbmontero@hotmail.com>
2022-07-06 17:39:57 -0300

    modificación router

--- client/router.ts
+++ client/router.ts
@@ -2,5 +2,6 @@ import { Router } from "@vaadin/router";
 
 const router = new Router(document.querySelector(".root"));
 router.setRoutes([
-    {path: "/", component: "home-comp"}
+    {path: "/", component: "home-comp"},
+    {path: "/home", component: "home-comp"}
 ]);
